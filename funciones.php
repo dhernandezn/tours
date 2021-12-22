839bb043b0940a20d6b440a83c6bfd4713bf0752
dhernandezn <d.hernaranjo@gmail.com>
2021-12-22 08:54:42 -0300

    Cambio de index, creación log_desarrollo, creación mod carg csv (etapa desarrollo)

--- funciones.php
+++ funciones.php
@@ -39,6 +39,67 @@ class Consultas
         exit();
     }
 
+    public function sub_res(){
+        
+            try {
+                $tipo		= $_FILES["csv_pep"]["type"];
+                $tamano		= $_FILES["csv_pep"]["size"];
+                $archtmp	= $_FILES["csv_pep"]["tmp_name"];
+                $lineas		= file($archtmp);
+        
+                echo $tipo;echo "</br>";
+                echo $tamano;echo "</br>";
+                echo $lineas;echo "</br>";
+        
+                if($tamano != 0){
+                    echo "Tiene peso";
+        
+                }else{
+                    echo "No pesa nada";
+                }
+        exit();	
+                $i=1;
+        
+                foreach($lineas as $linea){
+                    $cant_reg = count($lineas);
+                    $cant_reg_agr = ($cant_reg - 1);
+                    //echo $i;
+                    if($i != 0){
+                        $datos = explode(";",$linea);
+                        $id_pep = $datos[0];
+                        $id_pep = intval($id_pep);
+                        $rut_pep = $datos[1];
+                        $nom_pep = utf8_encode($datos[2]);
+                        // echo $id_pep." - ";
+                        // echo $rut_pep;
+                        // echo $nom_pep;
+                        // echo "</br>";
+        
+                        $cbd = Databasemysql::getInstance();
+                        $agrego = $cbd -> prepare("INSERT INTO reservas(pep_id,pep_rut,pep_nombre) VALUES (:v_id,:v_rt,:v_nom)");
+                        $agrego -> bindValue(':v_id',$id_pep);
+                        $agrego -> bindValue(':v_rt',$rut_pep);
+                        $agrego -> bindValue(':v_nom',$nom_pep);
+                        $agrego->execute();
+                    }
+                    $i++;
+                }
+                if(isset($agrego)){
+                    echo "Se han importado correctamente $cant_reg registros.";
+                }
+                else{
+                    echo "Nada para importar";
+                }
+                // echo "Lineas: ".$cant_reg;
+                // echo "Datos: ".$datos;
+        
+            } catch (PDOException $e) {
+                echo "ERROR: " . $e->getMessage();
+           }
+        
+        
+    }
+
 }
 
 
---
+++ subir.php
@@ -0,0 +1,103 @@
+<?php
+require_once('database.php');
+require_once("funciones.php");
+try {
+
+    //llenador de tabla
+    $cbd = Databasemysql::getInstance();
+    
+$mensaje = null;
+
+if (isset($_POST['insertar'])) {
+	$model = new Consultas();
+	$model -> csv_pep = htmlspecialchars($_POST["csv_pep"]);
+	$model -> sub_res();
+	$mensaje = $model -> mensaje; 
+
+}
+
+} catch (PDOException $e) {
+    echo "ERROR: " . $e->getMessage();
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cargar Reservas</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.4.5.0.js"></script>
+	<script src="js/js.js"></script>
+    
+    <script>
+    function cap(){
+        var a = '0';
+			a = document.getElementById("val1").value;
+			//b = document.getElementById("valu1").value;
+			console.log(a);
+            //mirar();
+			switch (a) {
+				case '1':
+					$("#editar_entrada").modal({backdrop:'static',keyboard:false});
+                       
+                    console.log("AUTOEX");
+					break;
+				default:
+					break;
+			}
+    };
+    </script>
+    <script>
+		window.onload=cap;
+	</script>
+</head>
+<body>
+<strong id="mensajes" value=""><?php echo $mensaje3; ?></strong>
+<input type="hidden" name="rut_cli" id="rut_cli" value="">
+    <div class="container-xl">
+        <div class="table-responsive">
+            <div class="table-wrapper">
+                <div class="table-title">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h2>Importar <b>Archivos</b></h2>
+                        <?php echo $resultado[""];?>
+                        </div>
+                        <div class="col-sm-6">
+                            <!-- <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Añadir nuevo colegio</span></a>-->
+                            <a href="show_all.php" class="btn btn-danger"><i class="material-icons">analytics</i> <span>Todos</span></a>
+                            <a href="index.php" class="btn btn-danger"><i class="material-icons">analytics</i> <span>Volver</span></a>
+                            <a href="subir.php" class="btn btn-danger"><i class="material-icons">analytics</i> <span>Importar</span></a>
+                        </div>
+                    </div>
+                </div>
+                <form action="<?php echo $_SERVER['PHP_SELF'];?>" class="form-horizontal" method="post" enctype="multipart/form-data" name="f" autocomplete="off">
+                <label for="file-input"><span>Seleccionar Archivo CSV para reservas...</span></label><br>
+                    <input type="file" name="csv_pep" id="file-input" accept=".csv" required>
+                    
+                    
+                    <div class="#">
+                        <input type="hidden" name="insertar"><hr>
+                        <input type="submit" class="boton_cons" onclick="checkRut()" name="login" value="Subir"><br><br>
+                        <!--<strong><?php //echo $mensaje; ?></strong>-->
+                            
+                    </div>
+			    </form>
+            </div>
+        </div> 
+    </div>
+   
+</body>
+</html>
